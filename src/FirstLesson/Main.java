52d67a324ccaa99bb798233af90ac834b2017c1c
Adnan Nassra <mohamad-adnan.nassar@adesso.de>
2022-04-10 16:22:19 +0200

    added: scanner for first lesson

--- src/FirstLesson/Main.java
+++ src/FirstLesson/Main.java
@@ -1,7 +1,17 @@
 package FirstLesson;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Lesson");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter nr1: ");
+        int nr1 = scanner.nextInt();
+
+        System.out.println("Enter nr2: ");
+        int nr2 = scanner.nextInt();
+
+        System.out.println("Result  = " + (nr1 + nr2));
     }
 }
