7cbe265c36805ab4bbf51f440d68612a669fa5e1
Adnan Nassra <mohamad-adnan.nassar@adesso.de>
2022-04-10 15:48:13 +0200

    edited: the structure of project

--- src/SecondLesson/Main.java
+++ src/SecondLesson/Main.java
@@ -1,7 +1,7 @@
-package FirstLesson;
+package SecondLesson;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Lesson");
+        System.out.println("Second Lesson");
     }
 }
